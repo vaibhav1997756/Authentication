88ef1860da5325833006a964a80aefaa0fb5974a
Vaibhav Singh <vaibhavsinghthebest@gmail.com>
2024-06-17 15:54:08 +0530

    authentication app

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express= require("express");
+const mysql = require("mysql");
+const cors =require("cors");
+
+const app=express();
+
+app.use(express.json());
+
+app.use(cors());
+
+const db= mysqlCreateConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'signup'
+})
+
+app.post('/signup',(req,res) =>{
+    const sql= "INSERT INTO login (`name`,`email`,`password`) VALUES (?)";
+    const values=[
+        req.body.name,
+        req.body.email,
+        req.body.password
+    ]
+
+})
+
+
+app.listen(8081,()=>{
+    console.log("listening");
+})
